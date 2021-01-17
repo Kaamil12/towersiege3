8663db85a6726de9490a4f5039b6270fbef4519c
Kaamil12 <69597401+Kaamil12@users.noreply.github.com>
2021-01-17 14:15:18 -0600

    Add files via upload

---
+++ box.js
@@ -0,0 +1,26 @@
+class Box extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+      this.image = loadImage("sprites/wood1.png");
+    }
+  
+  };
+  function score(){
+
+    block1.score();
+    block2.score();
+    block3.score();
+    block4.score();
+    block5.score();
+    block6.score();
+    block7.score();
+    block8.score();
+    block9.score();
+    block10.score();
+    block11.score();
+    block12.score();
+    block13.score();
+    block14.score();
+    block15.score();
+    block16.score();
+  }
